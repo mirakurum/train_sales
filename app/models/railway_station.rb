b3a09cb4710eb6f42480cf02df999e4d7dda222f
mirakurum <mirakuru.m@yandex.ru>
2017-07-17 01:28:00 +0400

    refactoring + small fix

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,5 +3,5 @@ class RailwayStation < ApplicationRecord
 
   has_many :trains, foreign_key: :current_station_id
   has_many :railway_stations_routes
-  has_many :routes, trough: :railway_stations_routes
+  has_many :routes, through: :railway_stations_routes
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -2,5 +2,5 @@ class Route < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }, uniqueness: true
 
   has_many :railway_stations_routes
-  has_many :railway_stations, trough: :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
 end
