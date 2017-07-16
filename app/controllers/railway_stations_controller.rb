a24c968cd1a85e858789187d4d99268252d294a4
mirakurum <mirakuru.m@yandex.ru>
2017-07-16 17:28:15 +0400

    add CRUD for railway stations

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -4,19 +4,40 @@ class RailwayStationsController < ApplicationController
     @railway_stations = RailwayStation.all
   end
 
+  def show
+    @railway_station = RailwayStation.find(params[:id])
+  end
+
   def new
     @railway_station = RailwayStation.new
   end
 
-  def create
-    @railway_station = RailwayStation.new(railway_station_params)
-
-    @railway_station.save
-    redirect_to @railway_station
+  def edit
+    @railway_station = RailwayStation.find(params[:id])
   end
 
-  def show
+  def create
+    @railway_station = RailwayStation.new(railway_station_params)
+    if @railway_station.save
+      redirect_to @railway_station
+    else
+      render 'new'
+    end
+  end
+
+  def update
     @railway_station = RailwayStation.find(params[:id])
+    if @railway_station.update(railway_station_params)
+      redirect_to @railway_station
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @railway_station = RailwayStation.find(params[:id])
+    @railway_station.destroy
+    redirect_to railway_stations_path
   end
 
   private
--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,2 +1,3 @@
 class RailwayStation < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3 }, uniqueness: true
 end
