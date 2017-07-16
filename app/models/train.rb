c02a6ede7e2fa083f813342c3d42225ecf4a2907
mirakurum <mirakuru.m@yandex.ru>
2017-07-16 21:47:07 +0400

    add ticket, user models with associations

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,4 +2,5 @@ class Train < ApplicationRecord
   validates :tnumber, presence: true, length: { minimum: 3 }, uniqueness: true
 
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  has_many :tickets
 end
---
+++ db/migrate/20170716173624_add_tickets_to_user.rb
@@ -0,0 +1,5 @@
+class AddTicketsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_belongs_to :tickets, :user
+  end
+end
